b08f748bbdbb4ae8546e4310a7510fb381e93ba0
George Ils <v.vince@yandex.ru>
2018-07-29 18:23:18 +0300

    get complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$('.carousel').carousel({
+  interval: 10000
+})
+
+$(document).ready(function () {
+  $("#thumbnail-preview-indicators").carousel({
+    interval: false
+  });
+});
 
 
 
@@ -12,19 +21,3 @@ $('.nav-item a').each(function () {
     });
 
 
-
-
-
-
-
-
-
-/*$(function () {  
-    $('.nav-item a').each(function () { 
-        var location = window.location.href; 
-        var link = this.href;  
-        if(location == link) { 
-            $(this).addClass('active');
-        }
-    });
-  });*/
